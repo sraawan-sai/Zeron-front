53fe7a1d1b958d88accde731d1c6ea67a96e89e1
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2025-02-21 15:34:41 +0530

    grids

---
+++ public/data/insightData.ts
@@ -0,0 +1,32 @@
+export const InsightData = [
+    {
+        "img": "/images/insight/#",
+        "title": "Cyber Value at Risk",
+        "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+        "img": "/images/insight/#",
+        "title": "Daily Risks Updates",
+        "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+        "img": "/images/insight/#",
+        "title": "Critical Internal Risks",
+        "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+        "img": "/images/insight/#",
+        "title": "Assets Criticality",
+        "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+        "img": "/images/insight/#",
+        "title": "Security Optimization Insights",
+        "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+        "img": "/images/insight/#",
+        "title": "Risk Matrix Prioritization",
+        "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+]
