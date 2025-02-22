7e8d52033d10172bb5115519e77d0f9117b487fc
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2025-02-22 12:37:24 +0530

    framer scroll based updates

--- app/home/components/Insights.tsx
+++ app/home/components/Insights.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import "@/styles/stars.css";
 
@@ -12,10 +12,20 @@ const fadeUp = {
   visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } },
 };
 
+const up = {
+  hidden: { y: 50 },
+  visible: { y: 0, transition: { duration: 0.6, ease: "easeOut" } },
+};
+
 const Insights = () => {
   return (
     <div className="flex flex-col items-center justify-center mx-auto gap-16 px-4 py-20 overflow-hidden">
-      <motion.div initial="hidden" whileInView="visible" viewport={{ once: false, amount: 0.2 }} variants={fadeUp}>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeUp}
+      >
         <Heading
           title="Decoding the Data Universe into Clear"
           subtitle="Bright Insights"
@@ -24,33 +34,47 @@ const Insights = () => {
         />
       </motion.div>
 
-      <motion.div className="w-full max-w-[600px] h-full max-h-[800px]" initial="hidden" whileInView="visible" viewport={{ once: false, amount: 0.2 }} variants={fadeUp}>
-        <div className="absolute w-full min-h-screen bottom-0 lg:-left-[100%]  clip-trap translate-y-[90%] bg-gradient-to-t from-[#0A0118] to-[#0A0118]/40 -z-1" />
-        <div className="absolute w-full min-h-screen bottom-0 lg:-left-[100%]  clip-trap translate-y-[90%] -z-2">
+      <div className="w-full h-full relative flex items-center justify-center">
+        <div className="absolute w-[100vw] min-h-screen bottom-0 clip-trap translate-y-[90%] bg-gradient-to-t from-[#0A0118] to-[#0A0118]/40 -z-1" />
+        <div className="absolute w-[100vw] min-h-screen bottom-0 clip-trap translate-y-[90%] -z-2">
           <section className="wrapper">
             <div id="stars"></div>
             <div id="stars2"></div>
             <div id="stars3"></div>
           </section>
         </div>
-        <video
-          src="/video/insights.mp4"
-          autoPlay={true}
-          muted
-          loop
-          className="w-full h-full object-cover object-center"
-        />
-      </motion.div>
+        <motion.div
+          className="w-full max-w-[600px] h-full max-h-[800px] relative"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.2 }}
+          variants={fadeUp}
+        >
+          <video
+            src="/video/insights.mp4"
+            autoPlay={true}
+            muted
+            loop
+            className="w-full h-full object-cover object-center"
+          />
+        </motion.div>
+      </div>
 
-      <motion.div className="mt-20 inline-grid grid-cols-1 md:grid-cols-3 gap-6" initial="hidden" whileInView="visible" viewport={{ once: false, amount: 0.2 }} variants={fadeUp}>
+      <motion.div
+        className="mt-20 inline-grid grid-cols-1 md:grid-cols-3 gap-6"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeUp}
+      >
         {InsightData.map((insight, index) => (
           <motion.div
             key={index}
             className="group w-full max-w-[400px] min-h-[300px] rounded-[16px] flex flex-col justify-between relative px-4 pt-3 pb-6 bg-[#0A0118]"
-            initial="hidden"
+            // initial="hidden"
             whileInView="visible"
-            viewport={{ once: false, amount: 0.2 }}
-            variants={fadeUp}
+            viewport={{ once: true, amount: 0.2 }}
+            variants={up}
           >
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[calc(100%+3px)] h-[calc(100%+3px)] rounded-[16px] -z-1 group-hover:bg-gradient-to-r from-[#1C76FD]/30 to-[#5721F0] transition-all duration-400" />
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[calc(100%+3px)] h-[calc(100%+3px)] rounded-[16px] -z-1 purplebg" />
