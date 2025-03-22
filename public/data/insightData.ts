ff57529425f6a4d12e60490c8d0a99fa72c3bd3e
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2025-03-22 12:38:03 +0530

    hero: insight card images

--- public/data/insightData.ts
+++ public/data/insightData.ts
@@ -1,31 +1,31 @@
 export const InsightData = [
     {
-        "img": "/images/insight/#",
+        "img": "/images/home/insights/card1.png",
         "title": "Cyber Value at Risk",
         "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     },
     {
-        "img": "/images/insight/#",
+        "img": "/images/home/insights/card2.png",
         "title": "Daily Risks Updates",
         "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     },
     {
-        "img": "/images/insight/#",
+        "img": "/images/home/insights/card3.png",
         "title": "Critical Internal Risks",
         "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     },
     {
-        "img": "/images/insight/#",
+        "img": "/images/home/insights/card4.png",
         "title": "Assets Criticality",
         "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     },
     {
-        "img": "/images/insight/#",
+        "img": "/images/home/insights/card5.png",
         "title": "Security Optimization Insights",
         "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     },
     {
-        "img": "/images/insight/#",
+        "img": "/images/home/insights/card6.png",
         "title": "Risk Matrix Prioritization",
         "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     },
